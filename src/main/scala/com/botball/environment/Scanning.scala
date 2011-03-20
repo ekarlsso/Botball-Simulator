ba855ae798a73c42cd19b353320c605bc2ae6ccd
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2011-03-20 23:03:43 +0200

    Change terminology and function visibility in Scanning

--- src/main/scala/com/botball/environment/Scanning.scala
+++ src/main/scala/com/botball/environment/Scanning.scala
@@ -3,9 +3,9 @@ package com.botball.environment
 case class ScannerData(nodes: List[Node]) extends SensorData
 
 trait Scanning extends Sensing {this: Node =>
-  override abstract def sense: List[SensorData] = ScannerData(seenNodes) :: super.sense
-  def seenNodes: List[Node] = {
+  override abstract def sense: List[SensorData] = ScannerData(foundNodes) :: super.sense
+  protected def foundNodes: List[Node] = {
     scene.nodes.filterNot( node => { node == this } )
   }
-  def scene: Scene = error("Not Implemented")
+  protected def scene: Scene = error("Not Implemented")
 }
