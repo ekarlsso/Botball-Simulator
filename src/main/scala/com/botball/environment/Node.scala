87a24b4ff09106f9ca5e373a5339aa3035e875d1
Erik Karlsson <erik.karlsson@iki.fi>
2011-03-23 20:45:34 +0200

    Changed Node to have only 2 position values (x, y), added rotation value and updated test

--- src/main/scala/com/botball/environment/Node.scala
+++ src/main/scala/com/botball/environment/Node.scala
@@ -2,23 +2,37 @@ package com.botball.environment
 
 import scalala.tensor.dense.DenseVector
 
-trait Static { this: Node =>
+trait Static {
+  this: Node =>
   protected def calculatePosition(timeDiff: Long): DenseVector[Double] = position
 }
 
-trait ConstantVelocityMovement { this: Node =>
-  protected def calculatePosition(timeDiff: Long): DenseVector[Double] = position :+ (velocity :* timeDiff.toDouble)
-  protected def velocity: DenseVector[Double] = DenseVector(0.0, 0.0, 0.0)
+trait ConstantVelocityMovement {
+  this: Node =>
+
+  protected def calculatePosition(timeDiff: Long): DenseVector[Double] =
+    position :+ (velocity :* timeDiff.toDouble)
+
+  protected def velocity: DenseVector[Double] = DenseVector(0.0, 0.0)
 }
 
-trait Animated { this: Node =>
-  override def evaluate(timetick: TimeTick) = new Node(calculatePosition(timetick.timeDiff))
+trait Animated {
+  this: Node =>
+
+  override def evaluate(timetick: TimeTick) =
+    new Node(calculatePosition(timetick.timeDiff), this.rotation)
+
   protected def calculatePosition(timeDiff: Long): DenseVector[Double]
 }
 
-class Node(pos: DenseVector[Double] = DenseVector(0.0, 0.0, 0.0)) extends DefaultSensing {
+class Node(pos: DenseVector[Double] = DenseVector(0.0, 0.0), rot: Double = 0.0)
+  extends DefaultSensing {
+
   def position = pos
+
+  def rotation = rot
+
   def evaluate(timetick: TimeTick) = this
 
-  def toJSON = "{\"type\": \"Node\", \"pos\":["+pos(0)+ ", "+pos(1)+", "+pos(2)+"]}"
+  def toJSON = "{\"type\": \"Node\", \"pos\":["+pos(0)+ ", "+pos(1)+"]}"
 }
--- src/test/scala/com/botball/environment/NodeSpecs.scala
+++ src/test/scala/com/botball/environment/NodeSpecs.scala
@@ -4,17 +4,19 @@ import org.specs.runner.JUnit4
 import org.specs.Specification
 import scalala.tensor.dense.DenseVector
 
+class ConstantVelocityNodeSpecsAsTests extends JUnit4(ConstantVelocityNodeSpecs)
+
 object ConstantVelocityNodeSpecs extends Specification {
   class ConstantVelocityNode extends Node with Animated with ConstantVelocityMovement
   "ConstantVelocityNode" should {
     "move with constant velocity" in {
-      testNode(DenseVector(1.0, 1.0, 0.0)).evaluate(timeDiff(4)).position.toList must beEqualTo(DenseVector(4.0, 4.0, 0.0).toList)
-      testNode(DenseVector(1.0, 1.0, 0.0)).evaluate(timeDiff(3)).position.toList must beEqualTo(DenseVector(3.0, 3.0, 0.0).toList)
-      testNode(DenseVector(2.0, 0.0, 1.0)).evaluate(timeDiff(3)).position.toList must beEqualTo(DenseVector(6.0, 0.0, 3.0).toList)
-      testNode(DenseVector(2.0, 0.0, 1.0), DenseVector(2.0, 1.0, 0.0)).evaluate(timeDiff(3)).position.toList must beEqualTo(DenseVector(8.0, 1.0, 3.0).toList)
+      testNode(DenseVector(1.0, 1.0)).evaluate(timeDiff(4)).position.toList must beEqualTo(DenseVector(4.0, 4.0).toList)
+      testNode(DenseVector(1.0, 1.0)).evaluate(timeDiff(3)).position.toList must beEqualTo(DenseVector(3.0, 3.0).toList)
+      testNode(DenseVector(2.0, 0.0)).evaluate(timeDiff(3)).position.toList must beEqualTo(DenseVector(6.0, 0.0).toList)
+      testNode(DenseVector(2.0, 0.0), DenseVector(2.0, 1.0)).evaluate(timeDiff(3)).position.toList must beEqualTo(DenseVector(8.0, 1.0).toList)
     }
   }
-  def testNode(vel: DenseVector[Double], pos: DenseVector[Double] = DenseVector(0.0, 0.0, 0.0)) = {
+  def testNode(vel: DenseVector[Double], pos: DenseVector[Double] = DenseVector(0.0, 0.0)) = {
     new ConstantVelocityNode {
       override def position = pos
       override protected def velocity = vel
